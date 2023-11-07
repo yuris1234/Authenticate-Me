d85041b9d295337967d9baf9d9bdb268af5426b9
yuris <yls4@cornell.edu>
2023-11-07 18:57:32 -0500

    complete phase 3 backend

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,5 +1,6 @@
 class Api::SessionsController < ApplicationController
   def show
+    # banana 
     if current_user 
       @user = current_user
       render 'api/users/show'
@@ -15,7 +16,6 @@ class Api::SessionsController < ApplicationController
     @user = User.find_by_credentials(username, password)
     if @user
       login!(@user)
-      # console.log('hello')
       render 'api/users/show'
     else
       render json: {errors: ['Invalid credentials']}, status: :unauthorized
